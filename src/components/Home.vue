a88bc86e07a6ead0b667b1d1b4e631c1694bccdb
ludwigcarlsson <zhendiac@gmail.com>
2020-07-14 13:27:52 +0200

    Added homepage with background

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home-page">
+      <img src="../assets/img/bg2.jpg" alt="background" class="background-image">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.home-page {
+    overflow: hidden;
+}
+.background-image {
+    width: 100%;
+    filter: blur(3px);
+    transform: scale(1.1);
+}
+</style>
