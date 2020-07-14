3cca3c6bac3dd9f67a19c92d0189038964d1319f
ludwigcarlsson <zhendiac@gmail.com>
2020-07-14 14:10:34 +0200

    Adjusted images and re-placed logo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home-page">
       <img src="../assets/img/bg2.jpg" alt="background" class="background-image">
+      <img src="../assets/img/loggor/harstuganlogo2vit.png" alt="logo" class="harstugan-image">
   </div>
 </template>
 
@@ -15,8 +16,15 @@ export default {
     overflow: hidden;
 }
 .background-image {
+    position: relative;
     width: 100%;
     filter: blur(3px);
     transform: scale(1.1);
+}
+.harstugan-image {
+    position: absolute;
+    top: 5%;
+    left: 50%;
+    transform: translate(-50%, -5%);
 }
 </style>
