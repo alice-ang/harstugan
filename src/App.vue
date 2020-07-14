04c504a9802d27c33afba627adbda2b3500ae0f6
a17alian <>
2020-07-14 15:51:42 +0200

    remove home component since its used in router view

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <Home msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue'
 
 
 export default {
   name: 'App',
   components: {
-    Home
+    
   }
 }
 </script>
