aa52b12a35c83099f8b1bb99a5fc72bd14a198e3
ludwigcarlsson <zhendiac@gmail.com>
2020-07-14 17:10:37 +0200

    Added carousel to homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,78 @@
 <template>
-  <div class="home-page">
-      <img src="../assets/img/bg2.jpg" alt="background" class="background-image">
-      <img src="../assets/img/loggor/harstuganlogo2vit.png" alt="logo" class="harstugan-image">
-  </div>
+    <div class="home-page">
+        <carousel-3d :controls-visible="true" class="carousel-3d">
+            <slide v-for="(slide, index) in gallery" v-bind:key="slide[index]" :index="index">
+                <img :src="slide.image" alt="image">
+            </slide>
+        </carousel-3d>
+    </div>
 </template>
 
 <script>
-export default {
+import { Carousel3d, Slide } from 'vue-carousel-3d';
 
-}
+export default {
+    data() {
+        return{
+            gallery: [
+                {
+                    image: require("../assets/img/galleribilder/img0.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img1.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img2.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img3.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img4.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img5.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img6.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img7.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img8.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img9.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img10.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img11.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img12.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img13.jpg"),
+                },
+                {
+                    image: require("../assets/img/galleribilder/img14.jpg"),
+                }
+            ]
+        }
+    },
+    components: {
+        Carousel3d,
+        Slide,
+    }
+};
 </script>
 
 <style>
-.home-page {
-    overflow: hidden;
+.carousel-3d {
+    width: 80% !important;
 }
-.background-image {
-    position: relative;
-    width: 100%;
-    filter: blur(3px);
-    transform: scale(1.1);
-}
-.harstugan-image {
-    position: absolute;
-    top: 5%;
-    left: 50%;
-    transform: translate(-50%, -5%);
-}
+
 </style>
