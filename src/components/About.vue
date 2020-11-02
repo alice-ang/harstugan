8a61da91710a6355551fbb9bdd7eed613c6bfae7
a17alian <>
2020-11-02 20:32:48 +0100

    changed layout on about page and added scrollable device for social media

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,17 +2,27 @@
   <div class="about-page">
     <h1>Om Hårstugan</h1>
     <div class="about-row">
-      <figure v-for="person in hairdresser" :key="person.name">
-        <img :src="person.url" alt="person" />
-        <figcaption>
-          <h5>{{ person.name }}</h5>
-        </figcaption>
-      </figure>
-    </div>
-    <div class="about-text">
-      <p>{{ about.text1 }}</p>
-      <p>{{ about.text2 }}</p>
-      <p>{{ about.text3 }}</p>
+      <div class="about-text">
+        <p>{{ about.text1 }}</p>
+        <p>{{ about.text2 }}</p>
+        <p>{{ about.text3 }}</p>
+        <div class="about-row">
+          <figure v-for="person in hairdresser" :key="person.name">
+            <img :src="person.url" alt="person" />
+            <figcaption>
+              <h5>{{ person.name }}</h5>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="smartphone">
+        <div class="content">
+          <iframe
+            src="https://www.facebook.com/v2.5/plugins/page.php?adapt_container_width=true&app_id=&channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df3466540a1e803c%26domain%3Dharstugan.se%26origin%3Dhttp%253A%252F%252Fharstugan.se%252Ff2e8838d6cda50c%26relation%3Dparent.parent&hide_cover=true&href=https%3A%2F%2Fwww.facebook.com%2FH%25C3%25A5rstugan-264806910204747%2F%3Ffref%3Dts&locale=en_US&sdk=joey&show_facepile=true&small_header=false&tabs=timeline"
+            style="width:100%;border:none;height:100%"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -51,6 +61,7 @@ export default {
 
 <style>
 .about-page {
+  padding: 1em;
   display: flex;
   flex-direction: column;
 }
@@ -75,15 +86,68 @@ figcaption {
 }
 
 .about-text {
-  margin: 0 1em;
+  padding: 0 1em;
+}
+/* The device with borders */
+.smartphone {
+  position: relative;
+  width: 290px;
+  height: 570px;
+  margin: auto;
+  border: 16px #2b2b2b solid;
+  border-top-width: 60px;
+  border-bottom-width: 60px;
+  border-radius: 36px;
+}
+
+/* The horizontal line on the top of the device */
+.smartphone:before {
+  content: "";
+  display: block;
+  width: 60px;
+  height: 5px;
+  position: absolute;
+  top: -30px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #333;
+  border-radius: 10px;
+}
+
+/* The circle on the bottom of the device */
+.smartphone:after {
+  content: "";
+  display: block;
+  width: 35px;
+  height: 35px;
+  position: absolute;
+  left: 50%;
+  bottom: -65px;
+  transform: translate(-50%, -50%);
+  background: #505050;
+  border-radius: 50%;
+}
+
+/* The screen (or content) of the device */
+.smartphone .content {
+  width: 250px;
+  height: 450px;
 }
 @media screen and (min-width: 905px) {
   .about-page {
     margin: 0 auto;
-    width: 70%;
+    width: 50%;
   }
   .about-row {
-    flex-wrap: no-wrap;
+    flex-wrap: nowrap;
+  }
+  .smartphone {
+    width: 370px;
+    height: 580px;
+  }
+  .smartphone .content {
+    width: 360px;
+    height: 460px;
   }
 }
 </style>
