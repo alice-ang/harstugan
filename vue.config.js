3a1daa355c7b701fb7f150baaf454d9ee6b24ff4
a17alian <>
2020-11-03 18:42:47 +0100

    trying to fix vonfig

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/production-sub-path/'
-      : '/'
-  }
+  // Project deployment base
+  // By default we assume your app will be deployed at the root of a domain,
+  // e.g. https://www.my-app.com/
+  // If your app is deployed at a sub-path, you will need to specify that
+  // sub-path here. For example, if your app is deployed at
+  // https://www.foobar.com/my-app/
+  // then change this to '/my-app/'
+  baseUrl: "/",
+
+  // where to output built files
+  outputDir: "dist",
+
+  // whether to use eslint-loader for lint on save.
+  // valid values: true | false | 'error'
+};
